093222c7b5df6c09d5880891e8c8bc1521332025
Adham <adam.bolotov@gmail.com>
2022-02-22 06:39:58 +0100

    clear

--- testUITaptick/ContentView.swift
+++ testUITaptick/ContentView.swift
@@ -1,18 +1,54 @@
-//
-//  ContentView.swift
-//  testUITaptick
-//
-//  Created by user213083 on 2/22/22.
-//
 
 import SwiftUI
 
+
+
+
 struct ContentView: View {
+    
+    
+    
+    @State var isStartingState: Bool = false
+
+    let generator = UINotificationFeedbackGenerator()
+    
+    
+    
+    
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        VStack {
+            Button("button") {
+            
+                self.generator.notificationOccurred(.success)
+            }
+       
+            Text("start le")
+          
+            RoundedRectangle(cornerRadius: 25)
+                .fill(Color.blue)
+            // if isStartingState true to Color.red  inache  Color.blue
+            
+            
+                    .frame(
+                        
+                        width: 200,
+                        height: 400
+   
+                    )
+            
+           
+            
+            Spacer()
+            
+        }
     }
 }
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
