6f45d2e183a20e306932027ea44abae9b1ef1319
Adham <adam.bolotov@gmail.com>
2022-02-22 06:16:47 +0100

    Initial Commit

---
+++ testUITaptick/testUITaptickApp.swift
@@ -0,0 +1,17 @@
+//
+//  testUITaptickApp.swift
+//  testUITaptick
+//
+//  Created by user213083 on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct testUITaptickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
