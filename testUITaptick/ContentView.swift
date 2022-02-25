0b890e74afdab8429c8acf67c2f502eda121bbf0
Adham <adam.bolotov@gmail.com>
2022-02-25 18:08:54 +0100

    before animating

--- testUITaptick/ContentView.swift
+++ testUITaptick/ContentView.swift
@@ -1,46 +1,63 @@
 // Tap heart
+// anti stress
 
 import SwiftUI
 
 
 struct ContentView: View {
     
-
+    
     let generator = UINotificationFeedbackGenerator()
-
+    
     @State var switchShadow: Bool = true
     
+    @State var isAnimated: Bool = false
+    
+    
+    /*
+     
+     добавить пружинную анимацию к сердцу   по нажатию   / быстрое сжатие и расширение - биение
+     
+     
+     извлечь фунцкиональность из тела
+     
+     
+     
+     на будущее
+     архив с нужными цветами и примененное к нему свойство рандом   назначение цвета
+     
+     */
     
     
     var body: some View {
         
         
-
+        
         ZStack {
             
             Rectangle()
                 .ignoresSafeArea()
-                .foregroundColor(switchShadow ? .black : .yellow)  //
+                .foregroundColor(switchShadow ? .black : .yellow)
             
-           
             
-        //    VStack {
-           
-                Button {
-                    
-                    self.generator.notificationOccurred(.success)
-                    
-                    switchShadow.toggle()
-                                         
-                    
-                } label: {
-                    Circle()
-                        .fill(switchShadow ? Color.white : Color.black)
-                        .frame(width: 150, height: 150)
-                    
-                        .shadow(color: switchShadow ? .white : .black, radius: 10)
-                    
-                        .overlay(
+            
+            Button {
+                
+                self.generator.notificationOccurred(.success)
+                switchShadow.toggle()
+                isAnimated.toggle()
+                
+            } label: {
+                
+                
+                
+                Circle()
+                    .fill(switchShadow ? Color.white : Color.black)
+                    .frame(width: 150, height: 150)
+                
+                    .shadow(color: switchShadow ? .white : .black, radius: 10)
+                
+                    .overlay(
                         Image(systemName: "heart.fill")
                             .resizable()
                             .aspectRatio(contentMode: .fit)
@@ -48,21 +65,20 @@ struct ContentView: View {
                             .foregroundColor(Color(hue: 1.0, saturation: 0.888, brightness: 0.74))
                         
                             .shadow(color: Color(hue: 1.0, saturation: 0.888, brightness: 0.74), radius: 10)
+//                            .animation(.spring(
+//                                response: 0.5,
+//                                dampingFraction: 0.7,
+//                                blendDuration: 1),
+//                                       value: isAnimating)
                         
-                        )
-                }
- 
-           // }
+                    )
+            }
         }
     }
 }
 
 
-// архив с нужными цветами и примененное к нему свойство рандом   назначение цвета
 
-// добавить пружинную анимацию к сердцу   по нажатию   / быстрое сжатие и расширение - биение
-
-// извлечь фунцкиональность из тела
 
 
 struct ContentView_Previews: PreviewProvider {
